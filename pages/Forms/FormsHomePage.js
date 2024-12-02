80312d72f871ec1789e3299125d2981082f41ea3
GrahamJReid <grahamreidtechnology@gmail.com>
2024-12-02 10:16:38 -0600

    patient forms page setup with tabs for various forms

--- pages/Forms/FormsHomePage.js
+++ pages/Forms/FormsHomePage.js
@@ -1,5 +1,6 @@
 /* eslint-disable react/button-has-type */
 import React from 'react';
+import { Tab, Tabs } from 'react-bootstrap';
 import { useAuth } from '../../utils/userContext';
 
 export default function FormsHomePage() {
@@ -19,6 +20,24 @@ export default function FormsHomePage() {
     <div>
       <h1>Forms Home Page</h1>
       <h2>Welcome, {user.username}!</h2>
+      <div>
+        <Tabs
+          defaultActiveKey="timelines"
+          id="uncontrolled-tab-example"
+        >
+          <Tab eventKey="intakeform" title="Intake Form">
+            <div>Patient Intake form</div>
+
+          </Tab>
+          <Tab eventKey="allergiesform" title="Allergies Form">
+            <div>allergies form</div>
+          </Tab>
+          <Tab eventKey="threads" title="Threads">
+            <div>Threads</div>
+
+          </Tab>
+        </Tabs>
+      </div>
     </div>
   );
 }
